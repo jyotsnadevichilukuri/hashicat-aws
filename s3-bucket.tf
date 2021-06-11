b6ce09429813490e870af1791f38b019d7a118d7
Jyotsna Chilukuri <jyotsnadevi_chilukuri@fanniemae.com>
2021-06-11 01:24:00 +0000

    last exercise

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/jyotsnach_training/s3-bucket/aws"
   version = "2.2.0"
-  bucket = "gaurav-s3-bucket"
+  bucket = "s3-bucket"
   # insert required variables here
   bucket_prefix = "jyotsnachilukuri"
 
