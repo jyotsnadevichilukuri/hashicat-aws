aadf849d45368caf1b64bc0a974af83b92ee8480
Jyotsna Chilukuri <jyotsnadevi_chilukuri@fanniemae.com>
2021-06-11 00:11:54 +0000

    Added s3 bucket module 1

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,7 @@
- module "s3-bucket" {
+module "s3-bucket" {
   source  = "app.terraform.io/jyotsnach_training/s3-bucket/aws"
   version = "2.2.0"
+  # insert required variables here
   bucket_prefix = "jyotsnachilukuri"
+
 }
- 
